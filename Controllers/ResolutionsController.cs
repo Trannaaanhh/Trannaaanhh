e571478b733ce5d5fd425ea628226c044bf9b069
Tran Duc Anh <121141638+Trannaaanhh@users.noreply.github.com>
2025-08-26 19:39:52 +0700

    Add files via upload

---
+++ Controllers/ResolutionsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NghiQuyetMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace NghiQuyetMVC.Controllers
+{
+    public class ResolutionsController : Controller
+    {
+        private readonly NghiQuyetContext _context;
+
+        public ResolutionsController(NghiQuyetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Resolutions
+        public IActionResult Index()
+        {
+            var resolutions = _context.Resolutions.ToList();
+            return View(resolutions);
+        }
+
+        // GET: /Resolutions/Details/{id}
+        public IActionResult Details(Guid id)
+        {
+            var resolution = _context.Resolutions
+                                     .Include(r => r.Tasks)
+                                     .FirstOrDefault(r => r.id == id);
+
+            if (resolution == null) return NotFound();
+
+            return View(resolution);
+        }
+    }
+}
---
+++ Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NghiQuyetMVC.Models;
+
+namespace NghiQuyetMVC.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly NghiQuyetContext _context;
+
+        public TasksController(NghiQuyetContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _context.TaskOverviews.ToListAsync();
+            return View(tasks);
+        }
+    }
+}
